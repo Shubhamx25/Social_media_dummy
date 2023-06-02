cae7d6026ad653149435930c37b4bf678ac85414
Shubhamx25 <shubhamy725p@gmail.com>
2023-06-02 18:27:20 +0530

    Eod 2

--- server/index.js
+++ server/index.js
@@ -18,5 +18,5 @@ app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}));
 app.use(morgan('common'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit:'30mb', extended: true}));
 
