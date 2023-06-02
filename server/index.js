58cb71ec267d1b7351f5a9b153d5e7513d70a0a0
Shubhamx25 <shubhamy725p@gmail.com>
2023-06-02 18:21:27 +0530

    EOD

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import multer from "multer";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Configurations and middlewares
+const __filename =  fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+let app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy:'cross-origin'}));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+
